20c77531d6bdde43c8937146ef971eba5e73200c
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-25 16:51:47 -0300

    change: add config middlewares of errors in node server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,17 @@
+import "express-async-errors";
 import express from "express";
 import "dotenv/config";
+import { globalsErrors } from "./middlewares/globals-errors";
+import { Bad_Request } from "./helpers/api-errors";
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("Ok!");
+server.use(express.json());
+
+server.post("/users", async (req, res) => {
+  throw new Bad_Request("caio no error");
 });
+
+server.use(globalsErrors);
 
 export { server };
