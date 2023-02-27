8aca8bdc8df7a803dc0a945842e1c562503ec07d
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-27 10:28:43 -0300

    feat: adding middleware of loginUser

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUserMiddleware } from "./server/middlewares/signUp/create-user";
+import { loginUserMiddlware } from "./server/middlewares/singIn/login-user";
 import { loginUserRouter } from "./server/useCases/signIn/login-user";
 import { createUserRouter } from "./server/useCases/signUp/create-user";
 
 const router = Router();
 
 router.post("/users", createUserMiddleware, createUserRouter);
-router.post("/login", loginUserRouter);
+router.post("/login", loginUserMiddlware, loginUserRouter);
 
 export { router };
---
+++ src/server/middlewares/singIn/login-user.ts
@@ -0,0 +1,41 @@
+import { RequestHandler } from "express";
+import { ILoginUserParams } from "../../controller/singIn/protocols";
+import { Bad_Request, Not_Fould } from "../../helpers/api-errors";
+import { comparePasswordHash } from "../../helpers/hashPassword";
+import { User } from "../../models/mongo-models/User";
+
+export const loginUserMiddlware: RequestHandler<
+  {},
+  {},
+  ILoginUserParams
+> = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Bad_Request("Email e password são obrigatórios.");
+  }
+
+  const allFieldsOfLogin: (keyof ILoginUserParams)[] = ["email", "password"];
+
+  const thereIsANon_RequiredField = Object.keys(req.body).some(
+    (key) => !allFieldsOfLogin.includes(key as keyof ILoginUserParams)
+  );
+
+  if (thereIsANon_RequiredField) {
+    throw new Bad_Request("So pode enviar os campos de email e passoword");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Not_Fould("Email invalido!");
+  }
+
+  const passwordHash = await comparePasswordHash(password, user.password);
+
+  if (!passwordHash) {
+    throw new Not_Fould("Senha invalida!");
+  }
+
+  next();
+};
