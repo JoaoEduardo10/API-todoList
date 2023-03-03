ff9660ec54f298087ed75b0e3c0edf5ee8bc5710
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-03 16:23:52 -0300

    feat: adding test middleware for delete-task

---
+++ test/middlewares/task/delete-task.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import { serverTest } from "../../globals-test";
+
+describe("update-complete-task middleware/upadate-complete-task", () => {
+  it("should returns 404 error from sending an id less than 24", async () => {
+    const { statusCode, body } = await serverTest
+      .delete("/tasks/1233")
+      .send({});
+
+    expect(statusCode).toBe(404);
+    expect(body).toEqual({ error: "id incompleto ou invalido" });
+  });
+});
