2cceed27f6f2525592a943ea06b429e33b2da759
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-05 10:29:45 -0300

    feat: ajust test when middlewares of update-subTask-task

--- test/middlewares/task/update-status-task.test.ts
+++ test/middlewares/task/update-status-task.test.ts
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { serverTest } from "../../globals-test";
 
--- test/middlewares/task/update-subTask-task.test.ts
+++ test/middlewares/task/update-subTask-task.test.ts
@@ -1,7 +1,22 @@
-import { describe, expect, it } from "vitest";
+import mongoose from "mongoose";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { serverTest } from "../../globals-test";
 
 describe("update-subTask middleware/update-subtask-task", () => {
+  beforeEach(async () => {
+    const url = process.env.MONGODB_URL as string;
+    const password = process.env.MONGODB_PASSWORD;
+    const username = process.env.MONGODB_USER;
+
+    mongoose.set("strictQuery", true);
+
+    await mongoose.connect(url, { auth: { password, username } });
+  });
+
+  afterEach(async () => {
+    await mongoose.connection.close();
+  });
+
   it("should retun an error for sending an in less than 24 characters with status code 404", async () => {
     const { statusCode, body } = await serverTest.patch(
       "/tasks/40028922/subtask"
@@ -48,5 +63,17 @@ describe("update-subTask middleware/update-subtask-task", () => {
 
     expect(statusCode).toBe(400);
     expect(body).toEqual({ error: "Id já usado ou esta com um campo a mais" });
+  });
+
+  it("should returns 404 error from sending a wue id does not exist", async () => {
+    const { statusCode, body } = await serverTest
+      .patch("/tasks/63ff9e27777e30323ed90a65/subtask")
+      .send([
+        { uuid: "123", concluded: false },
+        { uuid: "134", concluded: false },
+      ]);
+
+    expect(statusCode).toBe(404);
+    expect(body).toEqual({ error: "Id da invalido!" });
   });
 });
