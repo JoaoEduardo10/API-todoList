ec81b9205958d90b5afe195752151cb0c64fb41e
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-06 10:32:20 -0300

    feat: ajust test of delete-task

--- src/server/repositories/task/delete-task.ts
+++ src/server/repositories/task/delete-task.ts
@@ -1,4 +1,5 @@
 import { IDeleteTaskRepository } from "../../controller/task/protocols";
+import { Internal_Server_Error } from "../../helpers/api-errors";
 import { Task } from "../../models/mongo-models/Tasks";
 import { ITasks } from "../../models/protocols";
 
@@ -6,7 +7,13 @@ export class MongoDeleteTaskRepository implements IDeleteTaskRepository {
   async delete(id: string): Promise<ITasks> {
     const task = await Task.findByIdAndDelete(id);
 
-    const { _id, boardConnect, description, subTasks, text, status } = task!;
+    if (!task) {
+      throw new Internal_Server_Error(
+        "Eerro no banco de dados ao deletar a task"
+      );
+    }
+
+    const { _id, boardConnect, description, subTasks, text, status } = task;
 
     return {
       id: _id.toHexString(),
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   test: {
     setupFiles: ["./test/setup.ts"],
     environment: "node",
+    coverage: {
+      exclude: [
+        "src/server/helpers/*.ts",
+        "test/**/**/*.ts",
+        "src/server/types/*.ts",
+        "src/server/models/*.ts",
+        "src/server/models/**/*.ts",
+      ],
+    },
   },
 });
