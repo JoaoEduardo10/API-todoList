ddc50f43ea81710b2801c4fc050acb515012c08e
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-06 15:08:53 -0300

    feat: ajust test of middleware/get-task

--- test/middlewares/task/get-task.test.ts
+++ test/middlewares/task/get-task.test.ts
@@ -1,7 +1,22 @@
-import { describe, expect, it } from "vitest";
-import { serverTest } from "../../globals-test";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { Task } from "../../../src/server/models/mongo-models/Tasks";
+import { mockCreateTask, serverTest } from "../../globals-test";
+
+const task = {
+  id: "",
+};
 
 describe("get-task middleware/get-task", () => {
+  beforeEach(async () => {
+    const createTask = await Task.create(mockCreateTask);
+
+    task.id = createTask._id.toHexString();
+  });
+
+  afterEach(async () => {
+    await Task.deleteMany();
+  });
+
   it("shuold returns error for not seting id.length greantest wur 24 with status code 404", async () => {
     const { statusCode, body } = await serverTest.get(
       "/tasks/63ff9e27777e30323ed90a655"
@@ -18,5 +33,14 @@ describe("get-task middleware/get-task", () => {
 
     expect(statusCode).toBe(404);
     expect(body).toEqual({ error: "Task não existe!" });
+  });
+
+  it("should returns status codes 200 wiht a task", async () => {
+    const { statusCode, body } = await serverTest
+      .get(`/tasks/${task.id}`)
+      .send({});
+
+    expect(statusCode).toBe(200);
+    expect(body).toBeTruthy();
   });
 });
