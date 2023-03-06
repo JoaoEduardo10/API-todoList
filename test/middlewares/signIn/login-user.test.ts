c1983176d825d56411390d3a4bdba897d17cfa90
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-06 16:01:05 -0300

    feat: ajust test of middleware/task/create-user end login-user

--- test/middlewares/signIn/login-user.test.ts
+++ test/middlewares/signIn/login-user.test.ts
@@ -1,7 +1,31 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { createHashPassword } from "../../../src/server/helpers/hashPassword";
+import { User } from "../../../src/server/models/mongo-models/User";
 import { serverTest } from "../../globals-test";
 
+const user = {
+  email: "",
+  password: "",
+};
+
 describe("login-user middleware/login-user", () => {
+  beforeEach(async () => {
+    const password = await createHashPassword("123");
+
+    const createUser = await User.create({
+      email: "test@gmail.com",
+      name: "test",
+      password,
+    });
+
+    user.email = createUser.email;
+    user.password = createUser.password;
+  });
+
+  afterEach(async () => {
+    await User.deleteMany();
+  });
+
   it("shuold return status 400 for not envied params", async () => {
     const { statusCode, body } = await serverTest.post("/login").send({});
 
@@ -20,5 +44,35 @@ describe("login-user middleware/login-user", () => {
     expect(body).toEqual({
       error: "So pode enviar os campos de email e passoword",
     });
+  });
+
+  it("shuold return status 404 with a password", async () => {
+    const { statusCode, body } = await serverTest.post("/login").send({
+      email: "test@gmail.com",
+      password: "1234",
+    });
+
+    expect(statusCode).toBe(404);
+    expect(body).toEqual({ error: "Senha invalida!" });
+  });
+
+  it("shuold return status 404 with a email invalid", async () => {
+    const { statusCode, body } = await serverTest.post("/login").send({
+      email: "teste@gmail.com",
+      password: user.password,
+    });
+
+    expect(statusCode).toBe(404);
+    expect(body).toEqual({ error: "Email invalido!" });
+  });
+
+  it("shuold return status 200 with a jwt", async () => {
+    const { statusCode, body } = await serverTest.post("/login").send({
+      email: "test@gmail.com",
+      password: "123",
+    });
+
+    expect(statusCode).toBe(200);
+    expect(typeof body).toBe("string");
   });
 });
--- test/middlewares/signUp/create-user.test.ts
+++ test/middlewares/signUp/create-user.test.ts
@@ -1,7 +1,12 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it } from "vitest";
+import { User } from "../../../src/server/models/mongo-models/User";
 import { serverTest } from "../../globals-test";
 
 describe("create-user middleware/signUp", () => {
+  afterEach(async () => {
+    await User.deleteMany();
+  });
+
   it("should returns statusCode 400 for not sending the createParams", async () => {
     const response = await serverTest.post("/users").send({});
 
@@ -22,5 +27,16 @@ describe("create-user middleware/signUp", () => {
     expect(response.body).toEqual({
       error: "Formato de email invalido. adicione um @gmail.com",
     });
+  });
+
+  it("should returns statusCode status codes 201 with a user", async () => {
+    const response = await serverTest.post("/users").send({
+      name: "eduardo",
+      email: "eduardo@gmail.com",
+      password: "123",
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toBeTruthy();
   });
 });
