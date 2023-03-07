afb0f5eb9596ea647ecf0a0b43a69508bfd502ec
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-07 15:32:14 -0300

    feat: adding initial config of swaggerIU

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,10 +3,18 @@ import express from "express";
 import "dotenv/config";
 import { globalsErrors } from "./middlewares/globals-errors";
 import { router } from "./router";
+import swagger from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const server = express();
 
 server.use(express.json());
+
+server.use(
+  "/api-todo-list/documentation",
+  swagger.serve,
+  swagger.setup(swaggerDocs)
+);
 
 server.use(router);
 
