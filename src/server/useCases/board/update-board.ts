bc06dbfe049987f6cdd0a1a932fad444ddb46897
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-31 10:31:18 -0300

    feat: adding updateBoardRouter

---
+++ src/server/useCases/board/update-board.ts
@@ -0,0 +1,18 @@
+import { RequestHandler } from "express";
+import { UpdateBoardController } from "../../controller/board/update-boards";
+import { IHttRequest } from "../../controller/protocols";
+import { MongoUpdateBoardRepository } from "../../repositories/board/update-board";
+
+export const boardRouter: RequestHandler = async (req, res) => {
+  const mongoUpdateBoardRepository = new MongoUpdateBoardRepository();
+
+  const updateBoardController = new UpdateBoardController(
+    mongoUpdateBoardRepository
+  );
+
+  const { body, statusCode } = await updateBoardController.handle(
+    req as IHttRequest<{ boardName: string }>
+  );
+
+  res.status(statusCode).json(body);
+};
